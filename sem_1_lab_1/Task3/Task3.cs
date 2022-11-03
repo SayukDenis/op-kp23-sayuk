c7266947909df45e0427235633320a1d77fbba7b
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:54:48 +0200

    Test scenarios added

--- sem_1_lab_1/Task3/Task3.cs
+++ sem_1_lab_1/Task3/Task3.cs
@@ -3,12 +3,29 @@ class Lab1
 {
     static public void Main(string[] args)
     {
+        /*Test 1
+         * x = a; n = 1;
+         * Expected value error
+         * Test 2
+         * x = 1; n = b;
+         * Expected value error
+         * Test 3
+         * x = a; n = b;
+         * Expected value error
+         * Test 4
+         * x = 5; n = -2;
+         * Expected value error
+         * Test 5
+         * x = 2; n = 5;
+         * Expected value (120,32)
+         * Test 6
+         * x = 5; n = 2;
+         * Expected value (2,25)
+         */
         int n, x;
         int resfactorial = 1;
         int xinpown = 1;
         Console.WriteLine(resfactorial);
         Console.WriteLine(xinpown);
-
-
     }
 }
