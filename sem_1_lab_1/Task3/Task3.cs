4e6f7bf33bef4c6530bc2b9fb5cdf44506d5fb04
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:57:03 +0200

    Full implementation of the program

--- sem_1_lab_1/Task3/Task3.cs
+++ sem_1_lab_1/Task3/Task3.cs
@@ -29,3 +29,56 @@ class Lab1
         Console.WriteLine(xinpown);
     }
 }
+using System;
+class Lab1
+{
+    static public void Main(string[] args)
+    {
+        /*Test 1
+         * x = a; n = 1;
+         * Expected value error
+         * Test 2
+         * x = 1; n = b;
+         * Expected value error
+         * Test 3
+         * x = a; n = b;
+         * Expected value error
+         * Test 4
+         * x = 5; n = -2;
+         * Expected value error
+         * Test 5
+         * x = 2; n = 5;
+         * Expected value (120,32)
+         * Test 6
+         * x = 5; n = 2;
+         * Expected value (2,25)
+         */
+                int n, x;
+        int resfactorial = 1;
+        int xinpown = 1;
+        bool IsNInt, IsXInt;
+        Console.WriteLine("Enter the number n");
+        IsNInt = int.TryParse(Console.ReadLine(), out n);
+        if (!IsNInt || n < 1)
+        {
+            Console.WriteLine("Enter the right number n");
+            return;
+        }
+        Console.WriteLine("Enter the number x");
+        IsXInt = int.TryParse(Console.ReadLine(), out x);
+        if (!IsXInt)
+        {
+            Console.WriteLine("Enter the right");
+            return;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            resfactorial *= i;
+            xinpown *= x;
+
+        }
+        Console.WriteLine(resfactorial);
+        Console.WriteLine(xinpown);
+    }
+}
+
