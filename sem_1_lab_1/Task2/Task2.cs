c415df70bd80eca5ebd9d91d9f289e7188d0f868
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:37:28 +0200

    Test scenarios added

--- sem_1_lab_1/Task2/Task2.cs
+++ sem_1_lab_1/Task2/Task2.cs
@@ -2,7 +2,26 @@ class Lab1
 {
     public static void Main(string[] args)
     {
-        int n=0;
+        /*Test1
+         * n = a
+         * Expected values is error
+         * Test 2
+         * n= -1
+         * Expected values is error
+         *Test 3
+         * n= 0.5
+         * Expected values is error
+         * Test 4
+         * n= 0
+         * Expected values is error
+         * Test 5
+         * n= 17
+         * Expected values is "prime number"
+         * Test 6
+         * n= 16
+         * Expected values is "not prime number"
+         */
+        int n =0;
         Console.WriteLine("It is not prime number");
         Console.WriteLine("It is prime number");
     }
