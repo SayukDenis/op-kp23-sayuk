67dd3dbb02ad01edd317eb37c45bfa88034733a5
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 17:15:36 +0200

    Test scenarios added

--- sem_1_lab_1/Task4/Task4.cs
+++ sem_1_lab_1/Task4/Task4.cs
@@ -4,6 +4,33 @@ class Lab1
 {
     static public void Main(string[] args)
     {
+        /*
+         * Test 1
+         * sinx = b,a = 10
+         * Expected value error
+         * Test 2
+         * sinx = 300,a = b
+         * Expected value error
+         * Test 3
+         * sinx = 0,a = 10
+         * Expected value 0
+         * Test 4
+         * sinx = 90,a = 10
+         * Expected value 0,9999
+         * Test 5
+         * sinx = 150,a = 10
+         * Expected value 0,5
+         * Test 6
+         * sinx = 210,a = 10
+         * Expected value -0,5
+         * Test 7
+         * sinx = 1000,a = 10
+         * Expected value -0,9848
+         * Test 8
+         * sinx = 360,a = 10
+         * Expected value 0
+         * Test 8
+         */
         int a;
         double x, grad = 0;
         double sinx = 0;
