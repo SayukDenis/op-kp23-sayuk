ec2b7659549b9c925a07a8526cfc1cbfd0f5344e
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:30:52 +0200

    Program on a the plugs stage

---
+++ sem_1_lab_1/Task2/Task2.cs
@@ -0,0 +1,9 @@
+class Lab1
+{
+    public static void Main(string[] args)
+    {
+        int n=0;
+        Console.WriteLine("It is not prime number");
+        Console.WriteLine("It is prime number");
+    }
+}
