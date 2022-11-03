0d107bcb7bd7a14971cd8efb257585a788d6a3a4
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:17:34 +0200

    Program on a plugs stage

---
+++ sem_1_lab_1/Task1/Task1.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        double a = 0;
+        double b = 0;
+        double dx = 0;
+        double x0 = 0;
+        double xk = 0;
+        double y, x;
+        int num = 0;
+        Console.WriteLine(x + ": \t" + y);
+    }
+}
