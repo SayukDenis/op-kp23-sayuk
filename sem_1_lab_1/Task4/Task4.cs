6b7daea3c3e04c382b0c8c12ae170aa96db69b5d
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 17:02:37 +0200

    Program on a the plugs stage

---
+++ sem_1_lab_1/Task4/Task4.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Lab1
+{
+    static public void Main(string[] args)
+    {
+        int a;
+        double x, grad = 0;
+        double sinx = 0;
+        int c = 1;
+        Console.WriteLine(sinx * c);
+    }
+}
