0a13ba4402882908a9256382652f897f260d0845
SayukDenis <chinazes.4822@gmail.com>
2022-11-03 16:46:39 +0200

    Program on a the plugs stage

---
+++ sem_1_lab_1/Task3/Task3.cs
@@ -0,0 +1,14 @@
+using System;
+class Lab1
+{
+    static public void Main(string[] args)
+    {
+        int n, x;
+        int resfactorial = 1;
+        int xinpown = 1;
+        Console.WriteLine(resfactorial);
+        Console.WriteLine(xinpown);
+
+
+    }
+}
