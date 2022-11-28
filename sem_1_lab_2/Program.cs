20758fcf4e0f45949ab1cc0915678ec25c310d08
SayukDenis <chinazes.4822@gmail.com>
2022-11-28 14:23:16 +0200

    Programm on a plug stage

---
+++ sem_1_lab_2/Program.cs
@@ -0,0 +1,17 @@
+using System;
+class Percolation
+{
+    static void Main(String[] args)
+    {
+        int[,] array;
+        int n = 0;
+    }
+    static int[,] init(int n) { }
+    static int[,] Open(int row, int col, int[,] array) { return null; }
+    static bool isOpen(int row, int col, int[,] array) { return true; }
+    static bool isFull(int row, int col, int[,] array) { return true; }
+    static int numberOfOpenSites(int[,] array) { return 0; }
+    static bool percolates(int[,] array) { return true; }
+    static void print(int[,] array) { }
+    static int[,] Result(int[,] array) { return null; }
+}
